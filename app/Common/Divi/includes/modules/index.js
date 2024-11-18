04284981f082516ac7ba5cf07222e42ea221bfd6
RyanCreator <ryan_salter92@hotmail.com>
2024-11-18 12:34:20 +0000

    Delete app/Common directory

--- app/Common/Divi/includes/modules/index.js
+++
@@ -1,8 +0,0 @@
-import BookingPanel from './BookingPanel/BookingPanel';
-import SignIn from './SignIn/SignIn';
-import SignUp from './SignUp/SignUp';
-import ForgotPassword from './ForgotPassword/ForgotPassword';
-import BookingPopup from './BookingPopup/BookingPopup';
-import ChangeStatus from './ChangeStatus/ChangeStatus';
-
-export default [ BookingPanel , SignIn , BookingPopup , ChangeStatus, SignUp, ForgotPassword ];
